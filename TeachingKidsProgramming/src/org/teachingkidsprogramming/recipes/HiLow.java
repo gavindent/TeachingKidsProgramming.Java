531c6979f5d154472cf929be5d5ff2e527fa25f3
Jacob Leiken <jacobleiken@gmail.com>
2013-06-20 15:39:42 -0700

    Awesome HiLow -Jacob/Gavin

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/HiLow.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/HiLow.java
@@ -1,33 +1,58 @@
 package org.teachingkidsprogramming.recipes;
 
+import java.util.Random;
+
 import org.teachingextensions.logo.utils.Sounds;
 import org.teachingextensions.windows.MessageBox;
-
-import com.spun.util.NumberUtils;
 
 public class HiLow
 {
   public static void main(String[] args)
   {
-    int correctNum = NumberUtils.getRandomInt(1, 100);
-    for (int i = 1; i <= 8; i++)
+    int correctNum = new Random(100).nextInt(100);
+    int forVar = 8;
+    for (int i = 1; i <= forVar; i++)
     {
       int guess = MessageBox.askForNumericalInput("Guess a number");
-      if (guess == correctNum)
+      if (guess < 1 || 100 < guess)
+      {
+        MessageBox.showMessage("Your number " + guess + " is not a valid number or within the range");
+      }
+      else if (guess == correctNum)
       {
         Sounds.playBeep();
         MessageBox.showMessage("You won the game!");
         System.exit(0);
       }
-      else if (guess > correctNum)
+      else if (correctNum < guess)
       {
-        MessageBox.showMessage("Your number is too high");
+        int tries = forVar - i;
+        if (tries == 1)
+        {
+          String trs = "try";
+          MessageBox.showMessage("Your number is too high and you have " + (forVar - i) + " more " + trs);
+        }
+        else
+        {
+          String trs = "tries";
+          MessageBox.showMessage("Your number is too high and you have " + (forVar - i) + " more " + trs);
+        }
       }
       else if (guess < correctNum)
       {
-        MessageBox.showMessage("Your number is too low");
+        int tries = forVar - i;
+        if (tries == 1)
+        {
+          String trs = "try";
+          MessageBox.showMessage("Your number is too low and you have " + (forVar - i) + " more " + trs);
+        }
+        else
+        {
+          String trs = "tries";
+          MessageBox.showMessage("Your number is too low and you have " + (forVar - i) + " more " + trs);
+        }
       }
     }
-    MessageBox.showMessage("You lost the game");
+    MessageBox.showMessage("You lost the game and the answer was " + correctNum);
   }
 }
